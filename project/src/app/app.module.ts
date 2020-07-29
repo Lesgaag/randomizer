e921c30fd40dd177b250252787271a27c7e1ec24
lesgaag <leslievandergaag@gmail.com>
2020-07-29 17:09:52 +0200

    add navigator component

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigatorComponent } from './navigator/navigator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigatorComponent
   ],
   imports: [
     BrowserModule,
