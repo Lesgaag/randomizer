939161b7e57f287ea24409719701534802ee90e4
lesgaag <leslievandergaag@gmail.com>
2020-07-29 18:15:18 +0200

    add working routing

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { PossibilitiesPageComponent } from './possibilities-page/possibilities-page.component';
+import { RandomizerPageComponent } from './randomizer-page/randomizer-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutPageComponent },
+  { path: 'contact', component: ContactPageComponent },
+  { path: 'possibilities', component: PossibilitiesPageComponent },
+  { path: 'randomizer', component: RandomizerPageComponent },
+  { path: 'home', component: MainPageComponent },
+  { path: '**', component: MainPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
