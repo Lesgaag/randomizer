831b176340432bc182086dc8eba4cbae85afa1f0
lesgaag <leslievandergaag@gmail.com>
2020-08-26 15:38:24 +0200

    add hamburger menu on mobile

--- project/src/app/navigator/navigator.component.ts
+++ project/src/app/navigator/navigator.component.ts
@@ -9,7 +9,16 @@ export class NavigatorComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  // toggle hamburger view for mobile view
+  toggleMenu() {
+    const x = document.getElementById('myTopnav');
+    if (x.className === 'topnav') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'topnav';
+    }
   }
 
 }
