27d2a1220f99a947a42b638b248243854d9252d9
lesgaag <leslievandergaag@gmail.com>
2020-08-27 18:08:59 +0200

    adjust functionality and browser title

--- project/src/app/main-page/main-page.component.ts
+++ project/src/app/main-page/main-page.component.ts
@@ -1,10 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgModule } from '@angular/core';
+import { RandomizerPageComponent } from '../randomizer-page/randomizer-page.component';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
+
+@NgModule({
+  // Add component which should be available in main-page
+  declarations: [RandomizerPageComponent]
+})
+
 export class MainPageComponent implements OnInit {
 
   constructor() { }
